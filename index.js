8dd36f7fb0ef87784ddb2cc02f55259e09e056a3
Nemanja Dragaš <nemanjadragas@Nemanjas-MacBook-Pro.local>
2023-08-11 19:08:50 +0200

    body parser and cors added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const gost = require("./api/gost");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/gost", gost);
 
 app.listen(PORT, () => console.log("Server is running in port", PORT));
