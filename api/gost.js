e04e2e67dae949cf3e4583a00226ff89d72506c9
Nemanja Dragaš <nemanjadragas@Nemanjas-MacBook-Pro.local>
2023-08-11 18:56:56 +0200

    adding gost functions

--- api/gost.js
+++ api/gost.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { db } = require("../firebase-config");
 
-router.get("/", async (req, res) => {
+router.get("/get-brands", async (req, res) => {
   try {
     const brandsSnapshot = await db.ref("brands").once("value");
     const brandsData = brandsSnapshot.val();
@@ -13,6 +13,56 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.error("Error fetching brands:", error);
     res.status(500).json({ error: "Failed to fetch brands" });
+  }
+});
+
+router.post("/get-products", async (req, res) => {
+  const { brand } = req.body;
+
+  try {
+    const productsSnapshot = await db
+      .ref("products")
+      .orderByChild("brand")
+      .equalTo(brand)
+      .once("value");
+    const productsData = productsSnapshot.val();
+
+    if (!productsData) {
+      return res
+        .status(404)
+        .json({ error: "Products not found for this brand" });
+    }
+
+    const products = Object.values(productsData);
+
+    res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/get-product", async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const productSnapshot = await db
+      .ref("products")
+      .orderByChild("id")
+      .equalTo(id)
+      .once("value");
+    const productData = productSnapshot.val();
+
+    if (!productData) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    const product = Object.values(productData)[0];
+
+    return res.status(200).json(product);
+  } catch (err) {
+    console.error("Error getting product by id:", err);
+    return res.status(500).json({ error: "Failed to get product" });
   }
 });
 
