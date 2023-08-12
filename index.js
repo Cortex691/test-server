5fbdc5297bd25342fbb7b9523f7e80243629e86a
Nemanja Dragaš <nemanjadragas@Nemanjas-MacBook-Pro.local>
2023-08-12 16:20:06 +0200

    adding body parser and cors install

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const gost = require("./api/gost");
+
 const app = express();
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/gost", gost);
 
